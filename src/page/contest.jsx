667eeb80a8cc806ccdd16537d77c2f904a8c2368
cushybuy <adekola@paysnug.com>
2023-10-31 11:30:25 +0100

    better

--- src/page/contest.jsx
+++ src/page/contest.jsx
@@ -291,7 +291,7 @@ function Contest() {
                                   </div>
 
                                   <div align="center">
-                                        {data.contestVolume >= 200 ? `VOTE ${data.contestVolume}` : ''}
+                                        {data.contestVolume >= data.threshold ? `VOTE ${data.contestVolume}` : ''}
                                       </div>
                                   <button
                                     className='btn btn-success submit'
